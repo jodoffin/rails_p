a1727d744575b7884c3440940f28f12323c04472
Jeremy Odoffin <Jeremy.Odoffin@gmail.com>
2015-07-02 15:39:00 -1000

    CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  #get 'posts/index'
 
-  get 'welcome/about'
+  #get 'posts/show'
+
+  #get 'posts/new'
+
+  #get 'posts/edit'
+
+#  get 'welcome/index'
+  resources :posts
+
+  get 'about' => 'welcome#about'
+
 
   root to: 'welcome#index'
+
+
 end
 
 
